16201f104d9694f2c9aac5a923d6260a3430b538
gonzales-lloyd <lgonzalesna@nevada.unr.edu>
2021-11-11 18:41:54 -0500

    finish tests to driver

--- driver.cpp
+++ driver.cpp
@@ -4,6 +4,6 @@ int main (int argc, char *argv[]){
     std::string path = "test_files/CantinaBand3.wav";
     Wav wav_obj;
     wav_obj.readFile(path);
-    std::cout << sizeof(wav_obj.buffer);
+    std::cout << wav_obj.getBufferSize() << std::endl;
     return 0;
 } 
