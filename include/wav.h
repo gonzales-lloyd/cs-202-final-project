d3a79f097d977b17622aea0543ead58860d07eb1
gonzales-lloyd <lgonzalesna@nevada.unr.edu>
2021-11-11 19:07:32 -0500

    add doxygen setup

--- include/wav.h
+++ include/wav.h
@@ -3,11 +3,19 @@
 #include <string>
 #include "wavheader.h"
 
+/**
+ * Class for representing and performing file I/O on a WAV file.
+ */
 class Wav{
-public:
+public: //change to private as necessary
     wav_header header;
     unsigned char* buffer; //i.e. the sound data after the header
 public:
+    /**
+     * Read the WAV file at a certain path.
+     * 
+     * @param fileName The file's path.
+     */
     void readFile(const std::string &fileName);
     void writeFile(const std::string &fileName);
     int getBufferSize() const;
