14d8f946027d8cf3f57a8f5626505df6edd9f208
gonzales-lloyd <lgonzalesna@nevada.unr.edu>
2021-11-19 16:15:58 -0800

    finish tests for array behavior

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main (int argc, char *argv[]){
     Wav wav_obj;
     wav_obj.readFile(path);
     std::cout << std::hex << std::setfill('0') << std::setw(2) << wav_obj.buffer[0] << std::endl;
-    std::cout << std::dec;
+    //std::cout << std::dec;
     wavManipulation::gain_adjustment(wav_obj, 0.2);
     
     return 0;
--- src/wavmanip.cpp
+++ src/wavmanip.cpp
@@ -1,5 +1,7 @@
 #include "wavmanip.h"
 
-void wavManipulation::gain_adjustment(Wav& wav_obj, float scale){
-    std::cout << wav_obj.buffer[0] << std::endl;
+void wavManipulation::gain_adjustment(Wav &wav_obj, float scale){
+    //for(int i = 0; i<wav_obj.header.buffer_size; i++){
+    //    std::cout << wav_obj.buffer[i] << std::endl;
+    //}
 }
