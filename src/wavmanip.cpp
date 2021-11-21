7250553880e8b41f7717e44ebc1336695baee4b4
gonzales-lloyd <lgonzalesna@nevada.unr.edu>
2021-11-20 23:42:42 -0500

    basic gain adjustment with signed short

--- src/wavmanip.cpp
+++ src/wavmanip.cpp
@@ -1,7 +1,13 @@
 #include "wavmanip.h"
+#include <cmath>
 
 void wavManipulation::gain_adjustment(Wav &wav_obj, float scale){
-    //for(int i = 0; i<wav_obj.header.buffer_size; i++){
-    //    std::cout << wav_obj.buffer[i] << std::endl;
-    //}
+    for(int i = 0; i<wav_obj.header.buffer_size; i++){
+        auto sample = wav_obj.buffer[i];
+        if(sample<0){
+            wav_obj.buffer[i] = std::abs(wav_obj.buffer[i]) * -scale;
+        }else{
+            wav_obj.buffer[i] = std::abs(wav_obj.buffer[i]) * scale;
+        }
+    }
 }
