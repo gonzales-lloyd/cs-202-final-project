e751d46bc37a3aeb9729078dd81d1898492c33b4
Lloyd Gonzales <lgonzalesna@nevada.unr.edu>
2021-12-05 14:15:05 -0800

    remove whitespace from wav.h for consistency

--- include/wav.h
+++ include/wav.h
@@ -31,7 +31,6 @@
 class Wav{
 public:
     std::vector<std::vector<double>> audioData;
-
 private:
     wav_header header;
     unsigned char* buffer; //i.e. the sound data after the header - only used for file i/o
@@ -39,7 +38,6 @@ private:
     std::string fileName;
     
     int samplesPerChannel; //samples per channel - must be updated whenever a length-modifying action happens
-
 public:
     /**
      * @brief Get the audio format, where 1 indicates PCM and others indicate compression.
